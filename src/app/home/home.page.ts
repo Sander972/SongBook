3b36d9259c81571770e9a5e3e41958a29b63021a
Alessandro Minca <alessandro.minca@palazzetti.it>
2019-12-19 16:32:21 +0100

    added basic functionality to home, started song page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,14 @@
 import { Component } from '@angular/core';
+declare var require: any;
+var  jsonBook = require('../../assets/book.json');
 
+jsonBook.songs.forEach(el => {
+  if(el.text.length > 10){
+    //console.log(el.text.slice(0,40))
+    el["prem"] = el.text.slice(0,50) + "..."
+  }
+});
+//console.log(jsonBook);
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +16,12 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  book = jsonBook.songs;
+
+
   constructor() {}
+
+  ngOnInit(){
+  }
 
 }
